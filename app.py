bac23ef4f9bae1f15f7f852533e0f707c535792f
eshikapratheesh16 <eshikapratheeesh16@gmail.com>
2026-08-18 10:36:19 +0530

    Implement API endpoint for backend status
    Added an API endpoint to check the status of the mobile backend.

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello from PaaS Lab! Student: Eshika Pratheesh, Roll No:24MIC0061"
+@app.route('/api/v1/status', methods=['GET'])
+def get_status():
+    # Lab Part E Response (Modify this later in Part F)
+    return jsonify({
+        "status": "Mobile backend online",
+        "database": "connected",
+        "server_environment": "Render.com PaaS"
+    })
+
+if __name__ == '__main__':
+    # Render requires the app to bind to 0.0.0.0 and a dynamic port
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
